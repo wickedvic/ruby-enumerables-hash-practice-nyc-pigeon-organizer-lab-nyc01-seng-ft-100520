440b24d705d9d48f1e84fd1b540979b73ee41047
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 19:03:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
         end
 
         if !final_array[name][key]
-          final_array[name][key] = []
+          !final_array[name][key] = []
         end
 
         final_array[name][key].push(inner_key)

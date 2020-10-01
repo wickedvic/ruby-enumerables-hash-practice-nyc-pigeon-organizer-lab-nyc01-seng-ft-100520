6f0d898c31b8586c987c8dc7513c589a20847e74
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 18:59:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
  final_results = data.each_with_object ({}) do{ |key, value|, final_array}
+ 
     value.each do |new_value, names|
       names.each do |name|
         
